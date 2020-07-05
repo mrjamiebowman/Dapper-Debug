1d898e968fc08a75a35a7bacff03909d94af743b
Jamie Bowman <mrjamiebowman@gmail.com>
2020-07-05 19:47:10 -0400

    created sample project for testing.

---
+++ DapperDebug/SampleDapperDebug/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SampleDapperDebug.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ DapperDebug/SampleDapperDebug/Repository/SqlCustomerRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleDapperDebug.Repository
+{
+    public class SqlCustomerRepository : ICustomerRepository
+    {
+        // void create data table
+
+        // void proc create customer
+
+        // void proc get customers
+    }
+}
