8237b8be64b2e10a756a3aa1e7cbe52326560484
Jamie Bowman <mrjamiebowman@gmail.com>
2020-07-05 19:22:16 -0400

    initial commit

---
+++ DapperDebug/DapperDebug.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DapperDebug.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ DapperDebug/DapperDebug/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DapperDebug
+{
+    public class Class1
+    {
+    }
+}
